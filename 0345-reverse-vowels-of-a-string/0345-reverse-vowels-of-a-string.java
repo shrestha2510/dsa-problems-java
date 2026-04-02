5664643e6dc9aa4311510306d0b9c341f127068c
shrestha2510 <139290719+shrestha2510@users.noreply.github.com>
2026-04-02 12:23:18 +0530

    Time: 3 ms (77.78%), Space: 46.6 MB (74.75%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,22 @@
 class Solution {
     public String reverseVowels(String s) {
-        char[] arr = s.toCharArray();
-        int n = arr.length;
-        int p=0;
-        int q=n-1;
-        ArrayList<Character> a = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' || arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U'){
-                a.add(arr[i]);
-            }
+       char[] chars = s.toCharArray();
+       int left = 0;
+       int right = chars.length-1;
+       String vowels = "aeiouAEIOU";
+       while(left<right){
+        while(left<right && vowels.indexOf(chars[left])==-1){
+            left++;
         }
-        Collections.reverse(a);
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<n; i++){
-           if(arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' || arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U'){
-                sb.append(a.remove(0));
-            }
-            else{
-                sb.append(arr[i]);
-            }
+         while(left<right && vowels.indexOf(chars[right])==-1){
+            right--;
         }
-        String result = sb.toString();
-        return result;
+        char temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
+        left++;
+        right--;
+       }
+       return new String(chars);
     }
 }
